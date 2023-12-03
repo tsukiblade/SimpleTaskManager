eb4d1bf88acf63027b62ff3a83230154e2a08f37
Szymon Kozlowski <kierowcataczkipl@gmail.com>
2023-12-03 14:37:20 +0100

    Implementation

--- Program.cs
+++ Program.cs
@@ -9,27 +9,96 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 
 var app = builder.Build();
 
-var sampleTodos = new Todo[] {
-    new(1, "Walk the dog"),
-    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-    new(4, "Clean the bathroom"),
-    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
+var todoTasks = new List<TodoTask>
+{
+    new(1, "Walk the dog", "Take the dog for a walk around the block"),
+    new(2, "Do the dishes", "Make sure to do all the dishes in the sink", DateTime.Now.AddDays(1)),
+    new(3, "Do the laundry", "Make sure to do all the laundry in the basket", DateTime.Now.AddDays(3)),
+    new(4, "Clean the bathroom", "Make sure to clean the bathroom", DateTime.Now.AddDays(5)),
+    new(5, "Clean the car", "Make sure to clean the car", DateTime.Now.AddDays(7))
 };
 
-var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", () => sampleTodos);
+var todosApi = app.MapGroup("/tasks");
+
+todosApi.MapGet("/", () => todoTasks);
+
 todosApi.MapGet("/{id}", (int id) =>
-    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+    todoTasks.FirstOrDefault(a => a.Id == id) is { } todo
         ? Results.Ok(todo)
         : Results.NotFound());
 
+todosApi.MapPost("/", (TodoTaskDto todoDto) =>
+{
+    var newId = todoTasks.Max(a => a.Id) + 1;
+    var task = new TodoTask(newId, todoDto.Title, todoDto.Description, todoDto.DueBy);
+    
+    todoTasks.Add(task);
+    return Results.Created($"/tasks/{task.Id}", task);
+});
+
+todosApi.MapDelete("/{id}", (int id) =>
+{
+    var todo = todoTasks.FirstOrDefault(a => a.Id == id);
+    if (todo is null)
+    {
+        return Results.NotFound();
+    }
+
+    todoTasks.Remove(todo);
+    return Results.NoContent();
+});
+
+todosApi.MapPut("/{id}", (int id, UpdateTaskDto updateTaskDto) =>
+{
+    var task = todoTasks.FirstOrDefault(a => a.Id == id);
+
+    if (task is null)
+    {
+        var newId = todoTasks.Max(a => a.Id) + 1;
+        var newTask = new TodoTask(newId, updateTaskDto.Title, updateTaskDto.Description, updateTaskDto.DueBy);
+    
+        todoTasks.Add(newTask);
+        
+        return Results.Created($"/tasks/{newTask.Id}", newTask);
+    }
+    
+    task = task with
+    {
+        Title = updateTaskDto.Title,
+        Description = updateTaskDto.Description,
+        DueBy = updateTaskDto.DueBy,
+        Completed = updateTaskDto.Completed
+    };
+    
+    return Results.Ok(task);
+});
+
+todosApi.MapPost("/{id}/complete", (int id) =>
+{
+    var task = todoTasks.FirstOrDefault(a => a.Id == id);
+
+    if (task is null)
+    {
+        return Results.NotFound();
+    }
+
+    task = task with { Completed = true };
+    
+    return Results.Ok(task);
+});
+
 app.Run();
 
-public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
+public record TodoTask (int Id, string? Title, string? Description, DateTime? DueBy = null, bool Completed = false);
 
-[JsonSerializable(typeof(Todo[]))]
+public record TodoTaskDto (string? Title, string? Description, DateTime? DueBy = null);
+
+public record UpdateTaskDto (string? Title, string? Description, DateTime? DueBy = null, bool Completed = false);
+
+[JsonSerializable(typeof(TodoTask[]))]
+[JsonSerializable(typeof(List<TodoTask>))]
+[JsonSerializable(typeof(TodoTaskDto))]
+[JsonSerializable(typeof(UpdateTaskDto))]
 internal partial class AppJsonSerializerContext : JsonSerializerContext
 {
-
 }
